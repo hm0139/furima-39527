4c7ed9f8829536772406cb9187bbeb1bcd7e2915
hm0139 <m.kyohei0114@gmail.com>
2023-07-10 22:07:55 +0900

    rubocop実行

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,14 +11,14 @@ class OrdersController < ApplicationController
     @buy_delivery = BuyDelivery.new(order_params)
     if @buy_delivery.valid?
       item = Item.find(@buy_delivery.item_id)
-      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp.api_key = ENV['PAYJP_SECRET_KEY']
       Payjp::Charge.create(
-        amount: item.price,  # 商品の値段
-        card: params[:token],    # カードトークン
-        currency: 'jpy'                 # 通貨の種類（日本円）
+        amount: item.price, # 商品の値段
+        card: params[:token], # カードトークン
+        currency: 'jpy' # 通貨の種類（日本円）
       )
       @buy_delivery.save
-      return redirect_to root_path
+      redirect_to root_path
     else
       @item = Item.find(params[:item_id])
       render 'index', status: :unprocessable_entity
@@ -28,17 +28,19 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:buy_delivery).permit(:postal_code, :prefecture, :city, :addresses, :building, :phone_number).merge(token: params[:token],user_id: current_user.id,item_id: params[:item_id])
+    params.require(:buy_delivery).permit(:postal_code, :prefecture, :city, :addresses, :building, :phone_number).merge(
+      token: params[:token], user_id: current_user.id, item_id: params[:item_id]
+    )
   end
 
   def set_public_key
-    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
+    gon.public_key = ENV['PAYJP_PUBLIC_KEY']
   end
 
   def move_to_top
     item = Item.find(params[:item_id])
-    if !user_signed_in? || item.buy != nil || current_user.id == item.user.id
-      redirect_to root_path
-    end
+    return unless !user_signed_in? || !item.buy.nil? || current_user.id == item.user.id
+
+    redirect_to root_path
   end
 end
--- spec/factories/buy_delivery.rb
+++ spec/factories/buy_delivery.rb
@@ -1,12 +1,12 @@
 FactoryBot.define do
   factory :buy_delivery do
-    postal_code { "601-3205" }
+    postal_code { '601-3205' }
     prefecture { 1 }
-    city { "札幌市" }
-    addresses { "札幌123" }
-    building { "札幌ビル" }
+    city { '札幌市' }
+    addresses { '札幌123' }
+    building { '札幌ビル' }
     phone_number { Faker::Number.number(digits: 10) }
-    token { "ch_" + Faker::Lorem.characters(number: 29) }
+    token { 'ch_' + Faker::Lorem.characters(number: 29) }
     buy_id { 999 }
   end
 end
