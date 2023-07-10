dcbeba6fcf49190a23fde760ac164e7c4afd5e1a
hm0139 <m.kyohei0114@gmail.com>
2023-07-10 12:40:43 +0900

    購入機能の追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,19 +3,20 @@ class OrdersController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
-    @order = Buy.new
+    @buy_delivery = BuyDelivery.new
   end
 
   def create
-    @order = Buy.new(order_params)
-    if @order.valid?
+    @buy_delivery = BuyDelivery.new(order_params)
+    if @buy_delivery.valid?
+      item = Item.find(@buy_delivery.item_id)
       Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
       Payjp::Charge.create(
-        amount: params[:price],  # 商品の値段
+        amount: item.price,  # 商品の値段
         card: params[:token],    # カードトークン
         currency: 'jpy'                 # 通貨の種類（日本円）
       )
-      @order.save
+      @buy_delivery.save
       return redirect_to root_path
     else
       @item = Item.find(params[:item_id])
@@ -26,7 +27,7 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:buy).permit(:price).merge(token: params[:token],user_id: current_user.id,item_id: params[:item_id])
+    params.require(:buy_delivery).permit(:postal_code, :prefecture, :city, :addresses, :building, :phone_number).merge(token: params[:token],user_id: current_user.id,item_id: params[:item_id])
   end
 
   def set_public_key
---
+++ app/models/buy_delivery.rb
@@ -0,0 +1,20 @@
+class BuyDelivery
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :token, :postal_code, :prefecture, :city, :addresses, :building, :phone_number, :buy_id
+  
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :token
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture
+    validates :city
+    validates :addresses
+    validates :phone_number, format: {with: /\A[0-9]{10,11}\z/, message: "is invalid. Phone number must be between 10 and 11 digits"}
+  end
+
+  def save
+    buy = Buy.create(user_id: user_id, item_id: item_id)
+    Delivery.create(post_code: postal_code, prefecture: prefecture, municipality: city, address: addresses, building_name: building, telephone_number: phone_number, buy_id: buy.id)
+  end
+end
